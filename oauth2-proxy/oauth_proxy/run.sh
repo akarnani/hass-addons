4a9e337b83165bca1d652782d34cd778219aff4c
Andrew Karnani <andrew@Andrews-MBP.home>
2017-08-11 00:11:23 -0700

    first working config

---
+++ oauth2-proxy/oauth_proxy/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+CONFIG_PATH=/data/options.json
+
+secret=$(jq --raw-output ".client_secret" $CONFIG_PATH)
+id=$(jq --raw-output ".client_id" $CONFIG_PATH)
+cookie_secret=$(jq --raw-output ".cookie_secret" $CONFIG_PATH)
+cert=$(jq --raw-output ".certfile" $CONFIG_PATH)
+key=$(jq --raw-output ".keyfile" $CONFIG_PATH)
+jq --raw-output ".allowed_emails | .[]" $CONFIG_PATH > /data/emails
+
+/root/go/bin/oauth2_proxy --client-id $id \
+                --client-secret $secret \
+                --upstream http://172.17.0.1:8123 \
+                --cookie-secret $cookie_secret \
+                --cookie-refresh "1h" \
+                --redirect-url "http://localhost:4180/oauth2/callback" \
+                --tls-cert /ssl/$cert
+                --tls-key /ssl/$key
+                --authenticated-emails-file /data/emails
