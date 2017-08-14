419796ad59923fdd508ab55aa86d321d84ba630a
Andrew Karnani <andrew@akarnani.com>
2017-08-13 21:34:29 -0700

    use letsencrypt for tls

--- oauth2-proxy/run.sh
+++ oauth2-proxy/run.sh
@@ -6,12 +6,21 @@ id=$(jq --raw-output ".client_id" $CONFIG_PATH)
 cookie_secret=$(jq --raw-output ".cookie_secret" $CONFIG_PATH)
 cert=$(jq --raw-output ".certfile" $CONFIG_PATH)
 key=$(jq --raw-output ".keyfile" $CONFIG_PATH)
+domain=$(jq --raw-output ".domain" $CONFIG_PATH)
+email=$(jq --raw-output ".letsencryptAdminEmail" $CONFIG_PATH)
 jq --raw-output ".allowed_emails | .[]" $CONFIG_PATH > /data/emails
 
+
+/root/go/bin/redirect-https &
+
 /root/go/bin/oauth2_proxy --client-id $id \
+                --https-address ":443" \
                 --client-secret $secret \
                 --upstream http://172.17.0.1:8123 \
                 --cookie-secret $cookie_secret \
                 --cookie-refresh "1h" \
-                -skip-provider-button \
-                --authenticated-emails-file /data/emails
+                --skip-provider-button \
+                --authenticated-emails-file /data/emails \
+                --letsencrypt-enabled=true \
+                --letsencrypt-host $domain \
+                --letsencrypt-admin-email $email
